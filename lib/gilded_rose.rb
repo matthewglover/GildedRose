ebb66399f791ba5b9104771a858c57d8a02b0e86
Matt Glover <matthewglover@me.com>
2017-09-08 12:46:56 +0100

    Rename crnt_item to item

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -17,43 +17,43 @@ class GildedRose
   end
 
   def update_quality
-    @items.each do |crnt_item|
-      if standard_quality_item?(crnt_item)
-        crnt_item.quality -= 1 unless minimum_quality_item?(crnt_item)
+    @items.each do |item|
+      if standard_quality_item?(item)
+        item.quality -= 1 unless minimum_quality_item?(item)
       else
-        if (crnt_item.quality < 50)
-          crnt_item.quality = crnt_item.quality + 1
-          if (crnt_item.name == "Backstage passes to a TAFKAL80ETC concert")
-            if (crnt_item.sell_in < 11)
-              if (crnt_item.quality < 50)
-                crnt_item.quality = crnt_item.quality + 1
+        if (item.quality < 50)
+          item.quality = item.quality + 1
+          if (item.name == "Backstage passes to a TAFKAL80ETC concert")
+            if (item.sell_in < 11)
+              if (item.quality < 50)
+                item.quality = item.quality + 1
               end
             end
-            if (crnt_item.sell_in < 6)
-              if (crnt_item.quality < 50)
-                crnt_item.quality = crnt_item.quality + 1
+            if (item.sell_in < 6)
+              if (item.quality < 50)
+                item.quality = item.quality + 1
               end
             end
           end
         end
       end
-      if (crnt_item.name != "Sulfuras, Hand of Ragnaros")
-        crnt_item.sell_in = crnt_item.sell_in - 1;
+      if (item.name != "Sulfuras, Hand of Ragnaros")
+        item.sell_in = item.sell_in - 1;
       end
-      if (crnt_item.sell_in < 0)
-        if (crnt_item.name != "Aged Brie")
-          if (crnt_item.name != "Backstage passes to a TAFKAL80ETC concert")
-            if (crnt_item.quality > 0)
-              if (crnt_item.name != "Sulfuras, Hand of Ragnaros")
-                crnt_item.quality = crnt_item.quality - 1
+      if (item.sell_in < 0)
+        if (item.name != "Aged Brie")
+          if (item.name != "Backstage passes to a TAFKAL80ETC concert")
+            if (item.quality > 0)
+              if (item.name != "Sulfuras, Hand of Ragnaros")
+                item.quality = item.quality - 1
               end
             end
           else
-            crnt_item.quality = crnt_item.quality - crnt_item.quality
+            item.quality = item.quality - item.quality
           end
         else
-          if (crnt_item.quality < 50)
-            crnt_item.quality = crnt_item.quality + 1
+          if (item.quality < 50)
+            item.quality = item.quality + 1
           end
         end
       end
