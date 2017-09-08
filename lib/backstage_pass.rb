44494836e6486ef97c1bd34ed1f7a44ac882008b
Matt Glover <matthewglover@me.com>
2017-09-08 17:04:50 +0100

    Move methods to BackstagePass

--- lib/backstage_pass.rb
+++ lib/backstage_pass.rb
@@ -9,4 +9,18 @@ class BackstagePass < Item
       @quality = 0 if expired?
     end
   end
+
+  def increase_backstage_quality!
+    # NOTE: This code is not being tested
+    if sell_in < 11
+      unless maximum_quality?
+        increase_quality!
+      end
+    end
+    if sell_in < 6
+      unless maximum_quality?
+        increase_quality!
+      end
+    end
+  end
 end
