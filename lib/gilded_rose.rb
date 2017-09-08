d50a1bb5208d9fc59e2b8d50bb71c3886a07b270
Matt Glover <matthewglover@me.com>
2017-09-08 12:33:29 +0100

    Replace loop with each

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -18,47 +18,47 @@ class GildedRose
 
   def update_quality
 
-    for i in 0..(@items.size-1)
-      if (@items[i].name != "Aged Brie" && @items[i].name != "Backstage passes to a TAFKAL80ETC concert")
-        if (@items[i].quality > 0)
-          if (@items[i].name != "Sulfuras, Hand of Ragnaros")
-            @items[i].quality = @items[i].quality - 1
+    @items.each do |crnt_item|
+      if (crnt_item.name != "Aged Brie" && crnt_item.name != "Backstage passes to a TAFKAL80ETC concert")
+        if (crnt_item.quality > 0)
+          if (crnt_item.name != "Sulfuras, Hand of Ragnaros")
+            crnt_item.quality = crnt_item.quality - 1
           end
         end
       else
-        if (@items[i].quality < 50)
-          @items[i].quality = @items[i].quality + 1
-          if (@items[i].name == "Backstage passes to a TAFKAL80ETC concert")
-            if (@items[i].sell_in < 11)
-              if (@items[i].quality < 50)
-                @items[i].quality = @items[i].quality + 1
+        if (crnt_item.quality < 50)
+          crnt_item.quality = crnt_item.quality + 1
+          if (crnt_item.name == "Backstage passes to a TAFKAL80ETC concert")
+            if (crnt_item.sell_in < 11)
+              if (crnt_item.quality < 50)
+                crnt_item.quality = crnt_item.quality + 1
               end
             end
-            if (@items[i].sell_in < 6)
-              if (@items[i].quality < 50)
-                @items[i].quality = @items[i].quality + 1
+            if (crnt_item.sell_in < 6)
+              if (crnt_item.quality < 50)
+                crnt_item.quality = crnt_item.quality + 1
               end
             end
           end
         end
       end
-      if (@items[i].name != "Sulfuras, Hand of Ragnaros")
-        @items[i].sell_in = @items[i].sell_in - 1;
+      if (crnt_item.name != "Sulfuras, Hand of Ragnaros")
+        crnt_item.sell_in = crnt_item.sell_in - 1;
       end
-      if (@items[i].sell_in < 0)
-        if (@items[i].name != "Aged Brie")
-          if (@items[i].name != "Backstage passes to a TAFKAL80ETC concert")
-            if (@items[i].quality > 0)
-              if (@items[i].name != "Sulfuras, Hand of Ragnaros")
-                @items[i].quality = @items[i].quality - 1
+      if (crnt_item.sell_in < 0)
+        if (crnt_item.name != "Aged Brie")
+          if (crnt_item.name != "Backstage passes to a TAFKAL80ETC concert")
+            if (crnt_item.quality > 0)
+              if (crnt_item.name != "Sulfuras, Hand of Ragnaros")
+                crnt_item.quality = crnt_item.quality - 1
               end
             end
           else
-            @items[i].quality = @items[i].quality - @items[i].quality
+            crnt_item.quality = crnt_item.quality - crnt_item.quality
           end
         else
-          if (@items[i].quality < 50)
-            @items[i].quality = @items[i].quality + 1
+          if (crnt_item.quality < 50)
+            crnt_item.quality = crnt_item.quality + 1
           end
         end
       end
